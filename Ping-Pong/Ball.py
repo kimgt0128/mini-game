c0cb6aae553173cef498d917caf667fb67e1387a
KT <kimgt2828@gmail.com>
2024-04-24 15:30:16 +0900

    ball과 bar의 충돌 처리, 예외처리

--- Ping-Pong/Ball.py
+++ Ping-Pong/Ball.py
@@ -1,15 +1,22 @@
 import pygame
 
 class Ball:
-    def __init__(self, screen, color, tick):
+    def __init__(self, screen, color, time_sec, screen_width, screen_height):
         self.screen = screen
         self.color = color
-        self.time_sec = tick / 1000.0
-        self.x = 400
-        self.y = 300
+        self.time_sec = time_sec
+
+        self.speed_x = -screen_width / 1.28
+        self.speed_y = -screen_height / 1.92
 
         self.radious = 9
         self.diameter = self.radious * 2
 
+        self.x = 400
+        self.y = 300
     def draw_circle(self):
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radious)
+
+    def move(self):
+        self.x += self.speed_x * self.time_sec
+        self.y += self.speed_y * self.time_sec
--- Ping-Pong/rungame.py
+++ Ping-Pong/rungame.py
@@ -18,16 +18,22 @@ def runGame():
     screen_width = size[0]
     screen_height = size[1]
 
-    #탁구채 객체
+    
+
+    #탁구채, 탁구공 객체
     bar = Bar(screen, size[1], WHITE, size[1]/20)
-    ball = Ball(screen, WHITE, clock.tick(30))
+    ball = Ball(screen, WHITE, 0, size[0], size[1])
 
     # 탁구채 크기
     
 
     while not done:
+        time_passed = clock.tick(30)
+        time_sec = time_passed / 1000.0
         clock.tick(30)
         screen.fill(BLACK)
+
+        ball.time_sec = time_sec
         
         #keyboard 이벤트 처리
         for event in pygame.event.get():
@@ -36,19 +42,41 @@ def runGame():
                 break
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
-                    bar.direction = -10
+                    bar.direction = -20
                 elif event.key == pygame.K_s:
-                    bar.direction = 10
+                    bar.direction = 20
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_w:
                     bar.direction = 0
                 elif event.key == pygame.K_s:
                     bar.direction = 0
+        
+        #충돌이벤트 처리
+        #1. bar에 닿았을 때
 
+        if ball.x - ball.radious <= bar.width:
+            if (ball.y - ball.radious >= bar.positions[1]) and (ball.y - ball.radious <= bar.positions[1] + bar.height):
+                ball.x = bar.width + ball.radious
+                ball.speed_x = -ball.speed_x
+        #위, 아래, 오른쪽, 왼쪽 벽면에 닿았을 때
+        if ball.x - ball.radious <= 0:
+            done = True
+        elif ball.x + ball.radious > size[0]:
+            ball.speed_x = -ball.speed_x
+        if ball.y <= 0:
+            ball.speed_y = -ball.speed_y
+            ball.y = 0
+        elif ball.y >= size[1]:
+            ball.speed_y = -ball.speed_y
+            ball.y = size[1] - ball.radious
+
+
+        if ball.x - ball.radious <= bar.width:
+            print(bar.positions[1] + bar.height, " ", ball.y)
+        ball.move()
         bar.move()
         bar.draw_block()
         ball.draw_circle()
-        print(bar.positions[1])
         pygame.display.update()
 
 runGame()
